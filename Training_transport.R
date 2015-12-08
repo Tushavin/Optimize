9cbb60b0b2a8a49f0721135be5d513e4495dbf40
Amarest <Ivan-Mr.Bin-Petrov@yandex.ru>
2015-12-09 00:18:36 +0300

    Добавлен генератор транспортной задачи

---
+++ Training_transport.R
@@ -0,0 +1,23 @@
+#Генератор транспортной задачи
+
+
+z<-paste("Вариант ",1:10)
+set.seed(2015)
+tst<-list()
+library(knitr)
+for(i in 1:length(z)) {
+cat("\n### ",z[i],"\n")
+kolstrok=sample(5:10)
+kolstolbcov=kolstrok
+m<-matrix(sample(100:3000,2520,replace=T),nrow=kolstrok,ncol=kolstolbcov)
+rownames(m)<-paste0("A",1:nrow(m))
+colnames(m)<-paste0("A",1:ncol(m))
+m1<-m
+m1[lower.tri(m,diag=T)] <- 0
+m2<-t(m1)
+m<-m1+m2
+matrica<-m
+cat("\n\nНайти кратчайший путь\n\n")
+print(kable(matrica))
+tst[[lst]]<-matrica
+}
